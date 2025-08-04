fe9be13f5654dffaa63dc377ea6690b477ea6607
Fabricio Cappellari <fabricio.cappellari@thoughtworks.com>
2025-08-04 10:38:57 -0300

    feat (backend): Add new test for cache worker

---
+++ backend/tests/unit/workers/test_cache_worker.py
@@ -0,0 +1,26 @@
+import pytest
+from unittest.mock import MagicMock
+
+from app.worker import populate_cache_if_needed
+
+
+def test_populate_cache_calls_ai_service_when_quote_count_is_low(mocker):
+   """
+   Given: Number of quotes in the cache is below the threshold.
+   When: The cache worker is called.
+   Then: The AI service is called to generate quotes.
+   """
+   mock_redis_client = mocker.patch("app.worker.redis_client")
+   mock_redis_client.llen.return_value = 2
+   MINIMUM_QUOTE_COUNT = 5
+
+   mock_ai_quote_service = mocker.patch("app.worker.generate_ai_quotes")
+   mock_ai_quote_service.return_value = [
+      {"quote": "TDD is awesome.", "author": "A wise developer"}
+   ]
+
+   populate_cache_if_needed()
+   mock_ai_quote_service.assert_called_once()
+
+
+
